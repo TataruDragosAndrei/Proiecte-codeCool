ee06bef90bdc8936537a96badcadb7b0461c33f4
appuser <appuser@journey2-69bc5f84cc-5rmjd>
2022-08-30 13:48:27 +0000

    starter project

---
+++ Level1/level1.js
@@ -0,0 +1,9 @@
+/*
+  Move the prince through the maze to find the princess.
+  You can move the prince by calling the functions that
+  are already created:
+  `moveLeft`
+  `moveUp`
+  `moveDown`
+  `moveRight`
+*/
