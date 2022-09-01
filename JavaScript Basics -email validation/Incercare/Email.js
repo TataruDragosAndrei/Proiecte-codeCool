59c6fc610e818aa43e93983c10f2f4dbefe72071
Tataru Dragos <tatarudragos123@gmail.com>
2022-09-01 15:12:24 +0300

    1.09.2022

--- JavaScript Basics -email validation/Incercare/Email.js
+++ JavaScript Basics -email validation/Incercare/Email.js
@@ -1,13 +1,11 @@
 function ValidateEmail(input) {
 
- 
-
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-   var InvalidRegex1 = /^[@a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-   var InvalidRegex2 =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-   var InvalidRegex3 = /^@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-   var InvalidRegex4 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@/;
-   var InvalidRegex5 =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+   let InvalidRegex1 = /^[@a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+   let InvalidRegex2 =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+   let InvalidRegex3 = /^@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+   let InvalidRegex4 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@/;
+   let InvalidRegex5 =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\[a-zA-Z0-9-]+)*$)/;
     if (input.value.match(validRegex)) {
   
       alert("Valid email address!");
@@ -16,7 +14,7 @@ function ValidateEmail(input) {
   
       return true;
   
-    } else if (input.value.match(InvalidRegex1)) {
+    }else if (input.value.match(InvalidRegex1)) {
   
       alert(" An email address cannot contain more than one '@' characters!");
   
@@ -24,25 +22,29 @@ function ValidateEmail(input) {
   
       return false;
   
-    } else if (input.value.match(InvalidRegex2)) {
+    } 
+     if (input.value.match(InvalidRegex2)) {
       alert(" An email address has to contain a '@' character!");
   
       document.form1.text1.focus();
   
       return false;
-    } else if (input.value.match(InvalidRegex3)) {
+    }
+     if (input.value.match(InvalidRegex3)) {
       alert(" The username before the '@' character cannot be empty!");
   
       document.form1.text1.focus();
   
       return false;
-    } else if (input.value.match(InvalidRegex4)) {
+    }
+     if (input.value.match(InvalidRegex4)) {
       alert(" The domain after the '@' character cannot be empty!");
   
       document.form1.text1.focus();
   
       return false;
-    } else if (input.value.match(InvalidRegex5)) {
+    }
+     if (input.value.match(InvalidRegex5)) {
       alert(" An email address has to contain at least one '.' character!");
   
       document.form1.text1.focus();
